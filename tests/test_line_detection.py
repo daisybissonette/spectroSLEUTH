2091003f95170ae3e1da46f626932a953c171392
Daisy Bissonette <daisybissonette@dhcp-10-103-225-249.wireless.northwestern.private>
2024-07-18 10:38:43 -0500

    updated test for line detection

--- tests/test_line_detection.py
+++ tests/test_line_detection.py
@@ -1,1 +1,22 @@
-test here
+import numpy as np
+import matplotlib.pyplot as plt
+from pytest import approx
+from spectroSLEUTH.src.template import emission_lines
+
+def test_line_detection(): 
+    """
+    Test that evaluates the correctess of the emission_lines.line_detection() function. 
+    """   
+    
+    el = emission_lines('spec-0285-51930-0309.fits', 0.0646, promience=20)
+    el.results_table = el.line_detection()
+    print(el.results_table)
+    # assert that emission line at OIII is detection 
+    assert el.results_table['some OIII line'] is not None
+
+    # assert plotting doesn't fail 
+    fig, ax = cf.plot_best()
+    assert fig is not None
+    assert ax is not None
+
+    pass
